f6f0bb3b7168e48cb7cdd1c5a9dea9cafc01a5ca
taiton-k <84013946+taiton-k@users.noreply.github.com>
2024-04-03 02:25:11 +0000

    Update foot configuration.

--- home/foot.nix
+++ home/foot.nix
@@ -2,6 +2,11 @@
         programs.foot = {
                 enable = true;
                 server.enable = true;
+		settings = {
+                        main = {
+                                font = "PlemolJP Console NF:size=12";
+                        };
+                };
         };
 }
 
