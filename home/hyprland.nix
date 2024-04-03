8c85e3163294c3db5ce61c63697c2ef72b56be13
taiton-k <84013946+taiton-k@users.noreply.github.com>
2024-04-03 07:02:19 +0000

    Update Hyprland configuration.

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -20,7 +20,24 @@
                                 "vivaldi"
 				"foot"
                         ];
+                        decoration = {
+                                rounding = 8;
+                                inactive_opacity = 0.9;
+                                drop_shadow = true;
+                                shadow_range = 12;
+                                blur = {
+                                        enabled = true;
+                                        size = 8;
+                                };
+                        };
+			animation = [
+                                "global, 1, 3, default"
+			];
                         bind = [
+                                "$mod, Space, fullscreen, 1"
+                                "$mod, C, killactive"
+                                "$mod, Tab, cyclenext"
+
                                 "$mod, T, exec, foot"
                                 "$mod, V, exec, vivaldi"
 				"$mod, Q, exit"
